788773f3a690abc6a47176181fc1976c60551384
oenriquez <enriquez.oscar@outlook.com>
2015-09-26 19:50:38 -0500

    refactor oo fashion

--- main.cc
+++ main.cc
@@ -1,30 +1,67 @@
-// algoritmo de burbuja ORDENSAMIENTO DE NUMEROS  
+// algoritmo de burbuja ORDENAMIENTO DE NUMEROS  
 #include <iostream>
 #include <stdio.h>
-int main(int argc, char* argv[]){
-	int numeros[5] ={9,6,7,5,3};
-	int cambios_realizados = 0;
+//definicion de interfaz o abstrad 
+class Sortablelist{
+	public: 
+		virtual void swapWithNext(int index) = 0;
+		virtual bool isGratherThatNext(int index) = 0;
+		virtual int getzise() = 0;
+};
+// clase hija 
+class NumericList : public Sortablelist{
+	private:
+	 int* nums;
+	 int listsize;
+	public:
+		void swapWithNext(int index){
+			int temp = nums[index];
+			nums[index] = nums[index + 1];
+			nums[index + 1] = temp; 
+		}
+		bool isGratherThatNext(int index){
+			return (nums[index] > nums [index + 1]);
+		}
+		int getzise(void){
+			return listsize;
+		}
+		void setArray(int* nums, int size){
+			this->nums =  nums;
+			this->listsize = size;
+		}
+};
+
+void sort(Sortablelist* list){
+	// algoritmo de burbuja 
 	bool serealizadosCambios;
+	int listsize = list->getzise();
 	do{ 
 		serealizadosCambios = false;
-		int int_temp;
-		for (int i = 0; i < 4; ++i)
+		for (int i = 0; i < (listsize - 1); i++)
 		{
-			if (numeros[i] > numeros[i+1])
-				//cambios de posicion
+			if (list->isGratherThatNext(i))
 			{
-				int_temp = numeros[i];
-				numeros[i] = numeros[i+1];
-				numeros[i+1] = int_temp;
+				list->swapWithNext(i);
 				serealizadosCambios = true;
 			}
 		}
 
 	}while(serealizadosCambios);
 
+}
+
+int main(int argc, char* argv[]){
+	
+	NumericList list;
+	int numeros[5] ={9,6,7,5,3};
+	list.setArray(numeros,5);
+	sort(&list);
+	
+	
+	
+
 	for(int i = 0;i < 5;i++) 
 		std::cout<< "imprime numeros: "<< numeros[i] << std::endl;
 
-	
 	return 0;
 }
