ade3bc71e743cb90d79daa28b8bee9249ff657e8
oenriquez <enriquez.oscar@outlook.com>
2015-09-26 20:45:46 -0500

    use references instead of pointers

--- main.cc
+++ main.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 //definicion de interfaz o abstrad 
-class Sortablelist{
+class Sortablelist{ //padre 
 	public: 
 		virtual void swapWithNext(int index) = 0;
 		virtual bool isGratherThatNext(int index) = 0;
@@ -14,6 +14,9 @@ class NumericList : public Sortablelist{
 	 int* nums;
 	 int listsize;
 	public:
+		NumericList(int* nums,int size){
+			this->setArray(nums,size);
+		}
 		void swapWithNext(int index){
 			int temp = nums[index];
 			nums[index] = nums[index + 1];
@@ -30,38 +33,46 @@ class NumericList : public Sortablelist{
 			this->listsize = size;
 		}
 };
-
-void sort(Sortablelist* list){
+// Algoritmo de burbuja 
+void sort(Sortablelist& list){
 	// algoritmo de burbuja 
 	bool serealizadosCambios;
-	int listsize = list->getzise();
+	int listsize = list.getzise();
 	do{ 
 		serealizadosCambios = false;
 		for (int i = 0; i < (listsize - 1); i++)
 		{
-			if (list->isGratherThatNext(i))
+			if (list.isGratherThatNext(i))
 			{
-				list->swapWithNext(i);
+				list.swapWithNext(i);
 				serealizadosCambios = true;
 			}
 		}
 
 	}while(serealizadosCambios);
+}
+void print_array(int* array, int size){
+
+	for(int i = 0;i < (size);i++) 
+		std::cout<< "imprime numeros : "<< array[i] << std::endl;
+		std::cout<< std::endl;
 
 }
 
 int main(int argc, char* argv[]){
+	//polimorfidmo refecnaia al hijo  mediaante el padre  
+#define Array_sizes(var) (sizeof(var)/sizeof(int))
 	
-	NumericList list;
-	int numeros[5] ={9,6,7,5,3};
-	list.setArray(numeros,5);
-	sort(&list);
+	int numeros[] ={9,6,7,5,3,21,12,2,1,112,12,2,334,0};
+	NumericList list(numeros, Array_sizes(numeros));
+	sort(list);
+	print_array(numeros,Array_sizes(numeros));
 	
+	int numeros2[] ={9,6,7,5,3,21,12,2,1};
+	NumericList list2(numeros2, Array_sizes(numeros2));
+	print_array(numeros2,Array_sizes(numeros2));
+	sort(list2);
+	print_array(numeros2,Array_sizes(numeros2));
 	
-	
-
-	for(int i = 0;i < 5;i++) 
-		std::cout<< "imprime numeros: "<< numeros[i] << std::endl;
-
 	return 0;
 }
